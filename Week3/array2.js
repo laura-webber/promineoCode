5ec456afa0464809788d160e266bb619ccd75536
rodrigomaceno <maceno@gmx.com>
2022-06-13 23:02:46 -0600

    Created a new file with array methods

---
+++ Week3/array2.js
@@ -0,0 +1,56 @@
+
+// The map METHOD
+
+let names = ["Bob", "Clark", "Jason"];
+
+let lengths = names.map 
+(function(element){
+    return element.length;
+});
+
+console.log(lengths);
+
+// The reduce METHOD
+
+let sum = lengths.reduce(function(returnedValue, currentElement){
+    return returnedValue + currentElement;
+
+});
+console.log(sum);
+
+
+/*
+let sum = lengths.reduce(function(returnedValue, currentElement){
+    return returnedValue + currentElement;
+
+}, 1);
+console.log(sum);
+
+*/
+
+/*     The reduce METHOD with currentIndex and array as parameters
+
+let sum = lengths.reduce(function(returnedValue, currentElement, currentIndex, lengths){
+    return returnedValue + currentElement, lenghts[], arr;
+
+});
+console.log(sum);*/
+
+// The forEach METHOD
+
+names.forEach(function(element){
+    console.log(element);
+});
+
+// The filter METHOD  (returns a new array where the function invoked returned true)
+
+let fiveLetterNames = names.filter(function(element){
+    return element.length == 5;
+});
+
+console.log(fiveLetterNames);
+
+// The splice METHOD  can  ADD, MODIFY or REMOVE elements
+
+let removedElements = names.splice(0, 2);
+console.log(removedElements);

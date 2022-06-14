66fa906bf89cee848efa3ec5eb40816b28bd0149
rodrigomaceno <maceno@gmx.com>
2022-06-13 21:20:02 -0600

    Added a new function to display full name

--- Week3/function.js
+++ Week3/function.js
@@ -5,3 +5,25 @@ function myFunction () {
 }
 
 myFunction();
+
+/*
+function createFullName(firstName, lastName){
+    //var firstName = prompt("Please type your first name: ");
+    //var lastName = prompt("Please type your last name: ");
+    console.log(firstName + " " + lastName);
+}*/
+
+//createFullName("Bob", "Peterson");
+
+function createFullName(){
+    var firstName = prompt("Please type your first name: ");
+    var lastName = prompt("Please type your last name: ");
+    return firstName + " " + lastName;
+}
+
+function displayFullName(name){
+    console.log(name);
+}
+
+var fullName = createFullName();
+displayFullName(fullName);

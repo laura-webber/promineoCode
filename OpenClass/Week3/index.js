9b58dec46b88272d42fac8c28afedde9819413d8
rodrigomaceno <maceno@gmx.com>
2022-06-14 11:55:02 -0600

    Week 3 Open Class

---
+++ OpenClass/Week3/index.js
@@ -0,0 +1,33 @@
+/*function findCircumference(radius){
+    return Math.PI * 2 * radius;
+}
+
+var a = parseInt(prompt("Enter a value for the radius: "));
+console.log(findCircumference(a));
+*/
+
+/*
+var a = prompt("Please enter a color: ");
+
+function isBlue(color){
+    return color.toLowerCase() == "blue";
+}
+
+console.log(isBlue(a));
+*/
+
+/*
+var cardBalance = parseInt(prompt("Enter your card balance: "));
+var price = parseInt(prompt("Enter the price: "));
+
+function checkout(balance, price){
+    if(price > balance){
+        return false;
+    }
+    else {
+        return balance - price;
+    }
+}
+
+console.log(checkout(cardBalance, price));
+*/

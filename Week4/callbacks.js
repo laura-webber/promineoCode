c4dc4436f7cc2cb5c0096ae0bc9385e6896c43e2
rodrigomaceno <maceno@gmx.com>
2022-06-21 15:58:07 -0600

    .

--- Week4/callbacks.js
+++ Week4/callbacks.js
@@ -1,7 +1,19 @@
 let username = sendHttpRequest('getUser');
 console.log(username);
 
+// a FUNCTION is a functionality (It is convetion to USE verbs for function names and the function should do ONE task)
 
 // functions are OBJECTS
 // we can pass a FUNCTION into other functions as an ARGUMENT
 
+function logUsername(user){
+    console.log(user);
+}
+
+sendHttpRequest('getUser', logUsername);
+
+
+//               OR        we CAN DO THIS
+
+
+sendHttpRequest('getUser', user => console.log(user));

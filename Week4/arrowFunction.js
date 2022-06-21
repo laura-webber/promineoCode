61619e6d1774e19ed0b1203a8d9c44a4e074a953
rodrigomaceno <maceno@gmx.com>
2022-06-21 05:26:24 +0000

    Week 4

---
+++ Week4/arrowFunction.js
@@ -0,0 +1,14 @@
+let createFullName = (firstName, lastName) => `${firstName} ${lastName}`;
+
+console.log(createFullName('Bob', 'McKenzie'));
+
+let someFunction = (a, b) => {
+	let result = '';
+	for (let i = 0; i < b; i++){
+	result += a;
+	}
+	return result;
+}
+
+console.log(someFunction('Hi',3));
+

ff909f0d6e7dbf5bac0457f033643544b5c8cfbd
rodrigomaceno <maceno@gmx.com>
2022-06-21 04:58:06 +0000

    Week4

---
+++ Week4/templateliterals.js
@@ -0,0 +1,12 @@
+let myString = `Hi there!
+This is a new line.
+This is a second new line.`
+
+ console.log(myString);
+
+
+let anotherString = "Hi there!\nThis is a new line.\nThis is a second new line.";
+
+console.log(anotherString);
+
+console.log(`Six times three = ${6 * 3}`);

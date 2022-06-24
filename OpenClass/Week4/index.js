8586d79d796b5339d6b79d1f879e7d2232e98b7d
rodrigomaceno <maceno@gmx.com>
2022-06-24 03:17:01 +0000

    .

---
+++ OpenClass/Week4/index.js
@@ -0,0 +1,46 @@
+// function myFunction(yearlyPayment){
+//     let monthly = yearlyPayment / 12;
+//     return monthly.toFixed(2);
+// }
+
+
+// console.log(myFunction(12000));
+
+
+// const monthlyPayment = (yearlyPayment) => {
+//     return (yearlyPayment / 12).toFixed(2);
+// }
+
+// console.log(monthlyPayment(12000));
+
+
+
+
+// var a = parseInt(prompt("Enter your annual salary"));
+
+// let monthlyPayment = (yearlyPayment) =>{
+//     let monthly = yearlyPayment / 12;
+//     return monthly.toFixed(2);
+// };
+
+// console.log(monthlyPayment(a));
+
+
+function myFunction(num1, num2, num3){
+    let value = 0;
+    for (let i = 0; i < num3; i++) {
+        value = (value + num2) * num1;
+    }
+    return value / (num1 * (num3 * 10));
+}
+
+console.log(myFunction(2,3,1));
+
+
+const fancyAlgorithm = (num1, num2, num3) => {
+    let value = 0;
+    for (let i = 0; i < num3; i++) {
+        value = (value + num2) * num1;
+    }
+    return value / (num1 * (num3 * 10));
+}

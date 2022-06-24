b401d1ad2aebdc126b99fe125d6e170d5747252d
rodrigomaceno <maceno@gmx.com>
2022-06-24 03:41:55 +0000

    .

--- OpenClass/Week4/index.js
+++ OpenClass/Week4/index.js
@@ -26,21 +26,40 @@
 // console.log(monthlyPayment(a));
 
 
-function myFunction(num1, num2, num3){
-    let value = 0;
-    for (let i = 0; i < num3; i++) {
-        value = (value + num2) * num1;
-    }
-    return value / (num1 * (num3 * 10));
-}
+// function myFunction(num1, num2, num3){
+//     let value = 0;
+//     for (let i = 0; i < num3; i++) {
+//         value = (value + num2) * num1;
+//     }
+//     return value / (num1 * (num3 * 10));
+// }
 
-console.log(myFunction(2,3,1));
+// console.log(myFunction(2,3,1));
 
 
-const fancyAlgorithm = (num1, num2, num3) => {
-    let value = 0;
-    for (let i = 0; i < num3; i++) {
-        value = (value + num2) * num1;
-    }
-    return value / (num1 * (num3 * 10));
-}
+// const fancyAlgorithm = (num1, num2, num3) => {
+//     let value = 0;
+//     for (let i = 0; i < num3; i++) {
+//         value = (value + num2) * num1;
+//     }
+//     return value / (num1 * (num3 * 10));
+// }
+
+// function myFunction(num1, num2){
+//     return num1 ^ num2;
+// }
+
+// const xor = (num1, num2) =>  num1 ^ num2
+
+// console.log(xor(3,2));
+
+
+// let myFunction = num1 => num1;
+
+let myFunction = num1 => num1
+// NOT return num1;
+
+
+function addTwoInputs(a,b) { return a+b; }
+
+const returnSum = (a,b) => a + b;

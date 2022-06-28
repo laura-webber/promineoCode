5cc175a53009533a0dabf809b3702e4c7dbafd52
rodrigomaceno <maceno@gmx.com>
2022-06-28 21:46:55 +0000

    created class Student

--- Week5/index.js
+++ Week5/index.js
@@ -1,3 +1,19 @@
+// blueprint for student
+
+// class Student {
+//     constructor() {
+//         this.firstName = 'Sam'
+//         this.lastName = 'Smith'
+//         this.phoneNumber = '5555555'
+//         this.grade = 'A'
+//     }
+// }
+
 class Student {
-    
+    constructor(firstName, lastName, phoneNumber, grade) {
+        this.firstName = firstName
+        this.lastName = lastName
+        this.phoneNumber = phoneNumber
+        this.grade = grade
+    }
 }

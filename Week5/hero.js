c4bde353c051ee26893bc8d492fc070f4bfff009
rodrigomaceno <maceno@gmx.com>
2022-06-29 01:57:33 +0000

    .

---
+++ Week5/hero.js
@@ -0,0 +1,79 @@
+// Using the object below
+// 1 console log the customer name
+// 2 console log using a template literal "shipping to 123 Main street Gotham NY 12345" 
+// 3 add a property of "orderNum" and set it to 111222, change age to 44, console.log the object
+
+const customer = {
+    name: "Toby",
+    age: 32,
+    address: {
+        street: "123 Main street",
+        city: "Gotham",
+        state: "NY",
+        zip: "12345"
+    }
+}
+
+console.log(customer.name)
+console.log(`shipping to ${customer.address.street} ${customer.address.city} ${customer.address.state} ${customer.address.zip}`)
+
+
+
+// ==================================
+
+// Write a class for a Hero 
+// - the Hero should have a property of name, health, and power
+// - the Hero should also have a method of attack 
+//   - attack should return power plus 5
+
+class Hero {
+    constructor(name, health, power){
+        this.name = name
+        this.health = health
+        this.power = power
+    }
+
+    attack(){
+        return power + 5
+    }
+}
+
+// Write another class that will inherit Hero and call it Villain
+// - the villain should have all the same properties as Hero
+// - add a method call magic
+//   - magic should return power multiplied by 2
+
+// class Villain extends Hero {
+//     constructor(name, health, power){
+//         super();
+//         this.name = name
+//         this.health = health
+//         this.power =power
+//     }
+
+//     magic(){
+//         return power * 2
+//     }
+// }
+
+class Villain extends Hero {
+    constructor(name, health, power){
+        super(name, health, power);
+    }
+
+    magic(){
+        return power * 2
+    }
+}
+
+// Create a hero and a villain and log out all of their properties
+
+let hero1 = new Hero('Gandalf','200','Levitate')
+let villain1 = new Villain('Sauron','230','Fire')
+
+console.log(hero1)
+console.log(villain1)
+
+// *Bonus - Write a loop that will have the hero(use attack) and villain(use magic) attack each other 
+//          reducing the amount of health left and console.log each round.
+
--- Week5/index.js
+++ Week5/index.js
@@ -16,4 +16,65 @@ class Student {
         this.phoneNumber = phoneNumber
         this.grade = grade
     }
+
+    // methods (function that belongs to an OBJECT/CLASS)
+    introduce() {
+        console.log(`${this.firstName} ${this.lastName} can be reached at ${this.phoneNumber}`)
+    }
+}
+
+let student1 = new Student('Tom', 'Sawyer', '44444444', 'B')
+let student2 = new Student('Bob', 'Smith', '33333333', 'F')
+
+student1.introduce()
+student2.introduce()
+
+class PromotionSender {
+    constructor(){
+
+    }
+
+    sendNotification(notification){
+        console.log('Sending: ' + notification);
+    }
+
+    findUsesWithStatus(status){
+        let users = getUsers(status)
+        return users
+    }
+
+    calculateDiscount(status){
+        if (status === 'GOLD'){
+            return .3
+
+        } else if (status === 'SILVER') {
+            return .15
+        } 
+        return 0
+    }
+}
+
+class CollectionsSender {
+    constructor(){
+
+    }
+
+    sendNotification(notification){
+        console.log('Sending: ' + notification);
+    }
+
+    findUsesWithStatus(status){
+        let users = getUsers(status)
+        return users
+    }
+
+    calculateFee(status){
+        if (status === 'OVERDUE'){
+            return 10
+
+        } else if (status === 'DELINQUENT') {
+            return 25
+        } 
+        return 5
+    }
 }

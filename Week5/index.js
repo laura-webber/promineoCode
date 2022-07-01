490ac10495e44e7258ad4e1665176b4b2ed7bd3f
rodrigomaceno <maceno@gmx.com>
2022-07-01 17:53:34 -0600

    changed index.js

--- Week5/index.js
+++ Week5/index.js
@@ -9,29 +9,29 @@
 //     }
 // }
 
-class Student {
-    constructor(firstName, lastName, phoneNumber, grade) {
-        this.firstName = firstName
-        this.lastName = lastName
-        this.phoneNumber = phoneNumber
-        this.grade = grade
-    }
+// class Student {
+//     constructor(firstName, lastName, phoneNumber, grade) {
+//         this.firstName = firstName
+//         this.lastName = lastName
+//         this.phoneNumber = phoneNumber
+//         this.grade = grade
+//     }
 
-    // methods (function that belongs to an OBJECT/CLASS)
-    introduce() {
-        console.log(`${this.firstName} ${this.lastName} can be reached at ${this.phoneNumber}`)
-    }
-}
+//     // methods (function that belongs to an OBJECT/CLASS)
+//     introduce() {
+//         console.log(`${this.firstName} ${this.lastName} can be reached at ${this.phoneNumber}`)
+//     }
+// }
 
-let student1 = new Student('Tom', 'Sawyer', '44444444', 'B')
-let student2 = new Student('Bob', 'Smith', '33333333', 'F')
+// let student1 = new Student('Tom', 'Sawyer', '44444444', 'B')
+// let student2 = new Student('Bob', 'Smith', '33333333', 'F')
 
-student1.introduce()
-student2.introduce()
+// student1.introduce()
+// student2.introduce()
 
-class PromotionSender {
-    constructor(){
-
+class NotificationSender {
+    constructor(status){
+        this.status = status
     }
 
     sendNotification(notification){
@@ -42,6 +42,21 @@ class PromotionSender {
         let users = getUsers(status)
         return users
     }
+}
+
+class PromotionSender extends NotificationSender {
+    constructor(status){
+        super(status);
+    }
+
+    // sendNotification(notification){
+    //     console.log('Sending: ' + notification);
+    // }
+
+    // findUsesWithStatus(status){
+    //     let users = getUsers(status)
+    //     return users
+    // }
 
     calculateDiscount(status){
         if (status === 'GOLD'){
@@ -54,19 +69,20 @@ class PromotionSender {
     }
 }
 
-class CollectionsSender {
-    constructor(){
-
+class CollectionsSender extends NotificationSender {
+    constructor(status){
+        super(status); 
     }
+    super
 
-    sendNotification(notification){
-        console.log('Sending: ' + notification);
-    }
+    // sendNotification(notification){
+    //     console.log('Sending: ' + notification);
+    // }
 
-    findUsesWithStatus(status){
-        let users = getUsers(status)
-        return users
-    }
+    // findUsesWithStatus(status){
+    //     let users = getUsers(status)
+    //     return users
+    // }
 
     calculateFee(status){
         if (status === 'OVERDUE'){
@@ -78,3 +94,11 @@ class CollectionsSender {
         return 5
     }
 }
+
+let user1 = new CollectionsSender("OVERDUE");
+user1.sendNotification("YOU OWE US $10!");
+console.log(user1.calculateFee("OVERDUE"));
+
+let user2 = new PromotionSender("GOLD");
+user2.sendNotification("YOU GET 30% OFF ON YOUR NEXT PURCHASE!");
+console.log(user2.calculateDiscount("GOLD"));
